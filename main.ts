c4138927ebe48dffb11af06f9a17b48da5af7994
bredamatos <105065939+bredamatos@users.noreply.github.com>
2026-02-04 12:00:10 +0000

    Small changes with variable handling

--- main.ts
+++ main.ts
@@ -1,20 +1,32 @@
+let m_dir = 0
+let m_esq = 0
 let P = 0
 let I = 0
 let D = 0
 let virar = 0
 let erro = 0
 let erroAnt = 0
-let vel = 40
-let Kp = 40
-let Ki = 0.25
-let Kd = 5
+let vel = 100
+let Kp = 150
+let Ki = 5
+let Kd = 70
 basic.forever(function () {
     erro = maqueen.readPatrol(maqueen.Patrol.PatrolLeft) - maqueen.readPatrol(maqueen.Patrol.PatrolRight)
     P = Kp * erro
     I = I + Ki * erro
     D = (erro - erroAnt) * Kd
     virar = P + (I + D)
-    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, vel + virar)
-    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, vel - virar)
+    m_esq = vel + virar
+    m_dir = vel - virar
+    if (m_esq > 0) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, m_esq)
+    } else {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, Math.abs(m_esq))
+    }
+    if (m_dir > 0) {
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, m_dir)
+    } else {
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, Math.abs(m_dir))
+    }
     erroAnt = erro
 })
